1fb3265f9b5d096463093093e3c68d0ef7c0bf89
zibrov <zibrov@nanocad.ru>
2025-02-16 13:05:14 +0300

    Add Library for working with the Fibonacci function

---
+++ scr/Function/Maths.php
@@ -0,0 +1,23 @@
+<?php
+declare(strict_types=1);
+
+namespace Zibrov\OtusPhp2025Hw4\ZibrovOleg\Function;
+
+class Maths
+{
+    public static function getFibonacci(int $number): string
+    {
+        $arNumber = [];
+        $num1 = 0;
+        $num2 = 1;
+
+        for ($i = 0; $i < $number; $i++) {
+            $arNumber[] = $num1;
+            $num3 = $num1 + $num2;
+            $num1 = $num2;
+            $num2 = $num3;
+        }
+
+        return implode(', ', $arNumber);
+    }
+}
